839a5158e4646f0a3396000717ec158345e2a2b0
liamA <37907909@qq.com>
2020-05-02 16:21:33 +0800

    feat: http ；

--- example/httpdemo/client2/main.go
+++ example/httpdemo/client2/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/go-liam/tracing/config"
 	"github.com/go-liam/tracing/jaeger"
 	"github.com/go-liam/util/request"
 	"github.com/opentracing-contrib/go-stdlib/nethttp"
 	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
-	otlog "github.com/opentracing/opentracing-go/log"
 	"io"
 	"io/ioutil"
 	"log"
@@ -19,7 +16,7 @@ import (
 
 var (
 	port = ":7203"
-	serverName ="api-ai"
+	serverName ="api-ai2"
 	hostPort = "127.0.0.1:6831"
 )
 var sv *jaeger.SvJeager
@@ -48,7 +45,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("head: %+v\n",r.Header)
 	name, _ := os.Hostname()
 	path := r.URL.Path
-	st1:="%s: host: %s, path: %s ,token: %s ,UserID: %s ,ip:%s"
+	st1:="%s: 33host: %s, path: %s ,token: %s ,UserID: %s ,ip:%s"
 	st := fmt.Sprintf(st1,serverName, name,path,r.Header.Get("Token"),r.Header.Get("UserID"),request.ClientIP(r))
 	io.WriteString(w, st)
 }
@@ -58,43 +55,39 @@ func runClient(tracer opentracing.Tracer) {
 	c := &http.Client{Transport: &nethttp.Transport{}}
 
 	// create a top-level span to represent full work of the client
-	span := tracer.StartSpan(serverName)
-	span.SetTag(string(ext.Component), serverName)
-	defer span.Finish()
-	ctx := opentracing.ContextWithSpan(context.Background(), span)
+	//span := tracer.StartSpan(serverName)
+	//span.SetTag(string(ext.Component), serverName)
+	//defer span.Finish()
+	//ctx := opentracing.ContextWithSpan(context.Background(), span)
 
 	req, err := http.NewRequest(
 		"GET",
 		"http://localhost:7201/",
 		nil,
 	)
+	// trace
+	span,req3 := sv.HttpTracerRequestInject(req,serverName)
 	if err != nil {
-		onError(span, err)
+		sv.OnError(span, err)
 		return
 	}
 
-	req = req.WithContext(ctx)
+	//req = req.WithContext(ctx)
 	// wrap the request in nethttp.TraceRequest
-	req, ht := nethttp.TraceRequest(tracer, req)
-	defer ht.Finish()
+	//req, ht := nethttp.TraceRequest(tracer, req)
+	//defer ht.Finish()
 
-	res, err := c.Do(req)
+	res, err := c.Do(req3)
 	if err != nil {
-		onError(span, err)
+		sv.OnError(span, err)
 		return
 	}
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		onError(span, err)
+		sv.OnError(span, err)
 		return
 	}
-	fmt.Printf("Received result: %s\n", string(body))
+	fmt.Printf("Received 222: %s\n", string(body))
 }
 
-func onError(span opentracing.Span, err error) {
-	// handle errors by recording them in the span
-	span.SetTag(string(ext.Error), true)
-	span.LogKV(otlog.Error(err))
-	log.Print(err)
-}
--- jaeger/http_middleware.go
+++ jaeger/http_middleware.go
@@ -19,26 +19,28 @@ func (sv *SvJeager) NewMiddlewareHandle(serverName string) http.Handler {
 	return nethttp.Middleware(tracer, http.DefaultServeMux)
 }
 
-func (sv *SvJeager) HttpTracerRequestInject( req *http.Request,serverName string) {
+func (sv *SvJeager) HttpTracerRequestInject( req *http.Request,serverName string) (opentracing.Span,*http.Request) {
 	if sv.config == nil || !sv.config.IsOpen {
-		return
+		//return nil
 	}
 	// create a top-level span to represent full work of the client
 	span := sv.Tracer.StartSpan(serverName)
 	span.SetTag(string(ext.Component), serverName)
 	defer span.Finish()
 	ctx := opentracing.ContextWithSpan(context.Background(), span)
-
-	req = req.WithContext(ctx)
-
+	log.Printf("req %+v\n",req)
+	req2 := req.WithContext(ctx)
+	log.Printf("req2 %+v\n",req2)
 	// wrap the request in nethttp.TraceRequest
-	req, ht := nethttp.TraceRequest(sv.Tracer , req)
+	req3, ht := nethttp.TraceRequest(sv.Tracer , req2)
+	log.Printf("req3 %+v\n",req3)
 	defer ht.Finish()
+	return span,req3
 }
 
 func (sv *SvJeager) OnError(span opentracing.Span, err error) {
+	log.Print("[ERROR]OnError ",err)
 	// handle errors by recording them in the span
 	span.SetTag(string(ext.Error), true)
 	span.LogKV(otlog.Error(err))
-	log.Print("[ERROR]",err)
 }
